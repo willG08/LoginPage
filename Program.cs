bd83b74970b25556d2b738a2834d42cee6ed1f17
Will Gunther <131411925+willG08@users.noreply.github.com>
2025-01-28 09:45:26 -0700

    Commit1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesLogin.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesLoginContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesLoginContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesLoginContext' not found.")));
+
+builder.Services.AddHttpContextAccessor();
+// Added code
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
+    options.Cookie.HttpOnly = true; // Ensure cookie is accessible only via HTTP
+    options.Cookie.IsEssential = true; // Make the session cookie essential
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+//enables session for httpcontext
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
